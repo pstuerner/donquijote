bbe0fcc8582ef600d2f5e91e35be3fc883647ba4
pstuerner <philipp.stuerner@web.de>
2022-08-31 20:53:56 +0000

    add database functionalities

---
+++ donquijote/db/mongodb.py
@@ -0,0 +1,175 @@
+import os
+
+from pymongo import MongoClient
+
+
+class Mongo:
+    def __init__(self) -> None:
+        self.client = MongoClient(os.environ["MONGO_URI"])
+        self.db = self.client[os.environ["MONGO_DB"]]
+
+
+class User(Mongo):
+    def __init__(self):
+        super().__init__()
+        self.col = self.db.user
+
+    def find(self, user_id):
+        return self.col.find_one({"user_id": user_id})
+
+    def find_all(self):
+        return list(self.col.find())
+
+    def update(self, user_id, update_dict):
+        self.col.update_one({"user_id": user_id}, update_dict)
+
+    def insert(self, user_id, name, n_words, reminder, sign_up):
+        self.col.insert_one(
+            {
+                "user_id": user_id,
+                "name": name,
+                "n_words": n_words,
+                "reminder": reminder,
+                "sign_up": sign_up,
+            }
+        )
+
+    def exists(self, user_id):
+        return (
+            True
+            if self.col.count_documents({"user_id": user_id}) > 0
+            else False
+        )
+
+
+class Vocabulary(Mongo):
+    def __init__(self):
+        super().__init__()
+        self.col = self.db.vocabulary
+
+    def sample(self, n_words):
+        return self.col.aggregate(
+            [{"$sample": {"size": n_words}}, {"$project": {"_id": 0}}]
+        )
+
+    def find(self, vocab_id):
+        return self.col.find_one({"vocab_id": vocab_id}, {"_id": 0})
+
+    def from_vocab_list(self, vocab_list):
+        return self.col.aggregate(
+            [
+                {"$match": {"vocab_id": {"$in": vocab_list}}},
+                {"$project": {"_id": 0}},
+            ]
+        )
+
+
+class Practice(Mongo):
+    def __init__(self):
+        super().__init__()
+        self.col = self.db.practice
+
+    def max_id(self):
+        try:
+            return self.col.find_one(sort=[("practice_id", -1)])["practice_id"]
+        except (KeyError, TypeError):
+            return -1
+
+    def find(self, user_id, timestamp):
+        dt_start = timestamp.replace(hour=0, minute=0, second=0, microsecond=0)
+        dt_end = timestamp.replace(
+            hour=23, minute=59, second=59, microsecond=0
+        )
+
+        return self.col.find_one(
+            {
+                "user_id": user_id,
+                "timestamp": {"$gte": dt_start, "$lte": dt_end},
+            }
+        )
+
+    def update(self, practice_id, update_dict):
+        self.col.update_one({"practice_id": practice_id}, update_dict)
+
+    def insert(self, practice_id, user_id, timestamp, vocabs):
+        self.col.insert_one(
+            {
+                "practice_id": practice_id,
+                "user_id": user_id,
+                "timestamp": timestamp,
+                "vocabs": vocabs,
+                "attempts": {str(v): 0 for v in vocabs},
+            }
+        )
+
+    def exists(self, user_id, timestamp, return_count=False):
+        dt_start = timestamp.replace(hour=0, minute=0, second=0, microsecond=0)
+        dt_end = timestamp.replace(
+            hour=23, minute=59, second=59, microsecond=0
+        )
+
+        if not return_count:
+            return (
+                True
+                if self.col.count_documents(
+                    {
+                        "user_id": user_id,
+                        "timestamp": {"$gte": dt_start, "$lte": dt_end},
+                    }
+                )
+                > 0
+                else False
+            )
+        else:
+            return self.col.count_documents(
+                {
+                    "user_id": user_id,
+                    "timestamp": {"$gte": dt_start, "$lte": dt_end},
+                }
+            )
+
+
+class SRS(Mongo):
+    def __init__(self):
+        super().__init__()
+        self.col = self.db.srs
+
+    def repeat(self, user_id, timestamp):
+        return self.col.aggregate(
+            [
+                {"$match": {"user_id": user_id, "next_learn": timestamp}},
+                {"$project": {"_id": 0}},
+            ]
+        )
+
+    def find(self, user_id, vocab_id):
+        return self.col.find_one(
+            {"user_id": user_id, "vocab_id": vocab_id}, {"_id": 0}
+        )
+
+    def update(self, user_id, vocab_id, update_dict):
+        self.col.update_one(
+            {"user_id": user_id, "vocab_id": vocab_id}, update_dict
+        )
+
+    def insert(self, user_id, vocab_id):
+        self.col.insert_one(
+            {
+                "user_id": user_id,
+                "vocab_id": vocab_id,
+                "level": 1,
+                "last_learn": None,
+                "next_learn": None,
+                "quick_repeat": False,
+            }
+        )
+
+    def exists(self, user_id, vocab_id):
+        return (
+            True
+            if self.col.count_documents(
+                {"user_id": user_id, "vocab_id": vocab_id}
+            )
+            > 0
+            else False
+        )
